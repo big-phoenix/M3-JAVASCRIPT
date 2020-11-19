ce33c857a43438f75c9c635a536b3263a0ce215a
Javi <bisistems@gmail.com>
2020-11-19 17:10:23 +0100

    Fase 3

--- js/ciudades.js
+++ js/ciudades.js
@@ -1,19 +1,29 @@
-let ciudad1 = prompt("Indroduc la ciudad1: ");
-let ciudad2 = prompt("Indroduc la ciudad2: ");
-let ciudad3 = prompt("Indroduc la ciudad3: ");
-let ciudad4 = prompt("Indroduc la ciudad4: ");
-let ciudad5 = prompt("Indroduc la ciudad5: ");
-let ciudad6 = prompt("Indroduc la ciudad6: ");
+let ciudad1 = prompt("Indroduc la ciudad1: "); // Introducimos y guardamos los datos en la variables
+let ciudad2 = prompt("Indroduc la ciudad2: "); // Introducimos y guardamos los datos en la variables
+let ciudad3 = prompt("Indroduc la ciudad3: "); // Introducimos y guardamos los datos en la variables
+let ciudad4 = prompt("Indroduc la ciudad4: "); // Introducimos y guardamos los datos en la variables
+let ciudad5 = prompt("Indroduc la ciudad5: "); // Introducimos y guardamos los datos en la variables
+let ciudad6 = prompt("Indroduc la ciudad6: "); // Introducimos y guardamos los datos en la variables
 
-let arrayCiutats = [];
+let arrayCiutats = []; // Array de ciudades
+let arrayCiutatsModificades = []; //Array de de ciudades modicando "a" por "4"
 
 document.getElementById("respuesta").innerHTML = ciudad1 +"-"+ ciudad2 +"-"+ ciudad3 +"-"+ ciudad4 +"-"+ ciudad5 +"-"+ ciudad6;
 
-arrayCiutats.push(ciudad1);
-arrayCiutats.push(ciudad2);
-arrayCiutats.push(ciudad3);
-arrayCiutats.push(ciudad4);
-arrayCiutats.push(ciudad5);
-arrayCiutats.push(ciudad6);
+/***************************************************** Fase 3 *******************************************************************/
 
-document.getElementById("respuesta").innerHTML += "<br><br><b>" +arrayCiutats.sort();
+arrayCiutats.push(ciudad1); // Agregamos la ciudade al array de Ciudades
+arrayCiutats.push(ciudad2); // Agregamos la ciudade al array de Ciudades
+arrayCiutats.push(ciudad3); // Agregamos la ciudade al array de Ciudades
+arrayCiutats.push(ciudad4); // Agregamos la ciudade al array de Ciudades
+arrayCiutats.push(ciudad5); // Agregamos la ciudade al array de Ciudades
+arrayCiutats.push(ciudad6); // Agregamos la ciudade al array de Ciudades
+
+
+    for(ciudad of arrayCiutats){
+        arrayCiutatsModificades.push(ciudad.replace(/a/g,"4")); // Guardamos en la nueva Array el resultado de la modificacion
+    }
+
+arrayCiutatsModificades.sort(); // Ordenamos alfanumerico
+
+document.getElementById("respuesta").innerHTML += "<br><br><b>" +arrayCiutatsModificades; //Mostramos por pantalla
