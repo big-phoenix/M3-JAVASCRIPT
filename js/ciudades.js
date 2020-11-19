a8fcee39e47e348ae603f54a6215305bbd993bed
Javi <bisistems@gmail.com>
2020-11-19 16:41:22 +0100

    Fase 2

--- js/ciudades.js
+++ js/ciudades.js
@@ -5,4 +5,15 @@ let ciudad4 = prompt("Indroduc la ciudad4: ");
 let ciudad5 = prompt("Indroduc la ciudad5: ");
 let ciudad6 = prompt("Indroduc la ciudad6: ");
 
-document.getElementById("respuesta").innerHTML = ciudad1 +"<br>"+ ciudad2 +"<br>"+ ciudad3 +"<br>"+ ciudad4 +"<br>"+ ciudad5 +"<br>"+ ciudad6;
+let arrayCiutats = [];
+
+document.getElementById("respuesta").innerHTML = ciudad1 +"-"+ ciudad2 +"-"+ ciudad3 +"-"+ ciudad4 +"-"+ ciudad5 +"-"+ ciudad6;
+
+arrayCiutats.push(ciudad1);
+arrayCiutats.push(ciudad2);
+arrayCiutats.push(ciudad3);
+arrayCiutats.push(ciudad4);
+arrayCiutats.push(ciudad5);
+arrayCiutats.push(ciudad6);
+
+document.getElementById("respuesta").innerHTML += "<br><br><b>" +arrayCiutats.sort();
