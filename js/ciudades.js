03b050787ba7e61b0fa50be42d799f56355dc6ff
Javi <bisistems@gmail.com>
2020-11-19 16:19:40 +0100

    Fase 1

---
+++ js/ciudades.js
@@ -0,0 +1,8 @@
+let ciudad1 = prompt("Indroduc la ciudad1: ");
+let ciudad2 = prompt("Indroduc la ciudad2: ");
+let ciudad3 = prompt("Indroduc la ciudad3: ");
+let ciudad4 = prompt("Indroduc la ciudad4: ");
+let ciudad5 = prompt("Indroduc la ciudad5: ");
+let ciudad6 = prompt("Indroduc la ciudad6: ");
+
+document.getElementById("respuesta").innerHTML = ciudad1 +"<br>"+ ciudad2 +"<br>"+ ciudad3 +"<br>"+ ciudad4 +"<br>"+ ciudad5 +"<br>"+ ciudad6;
